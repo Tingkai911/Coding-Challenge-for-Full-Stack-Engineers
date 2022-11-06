8dad018be0dc640a57d69d9f584b42e9fb37b1b7
Tingkai911 <tingkai911@gmail.com>
2022-11-06 19:04:55 +0800

    update sample images

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,33 +1,43 @@
 const products = [
     {
-        title: "Airpods Wireless Bluetooth Headphones",
+        title: "Bored Ape Yacht Club - Gold #232",
         sku: "1",
-        image: "/images/airpods.jpg"
+        image: "/images/bayc-232.jpg"
     },
     {
-        title: "iPhone 11 Pro 256GB Memory",
+        title: "Bored Ape Yacht Club - Rainbow #2087",
         sku: "2",
-        image: "/images/phone.jpg"
+        image: "/images/bayc-2087.jpg"
     },
     {
-        title: "Cannon EOS 80D DSLR Camera",
+        title: "Bored Ape Yacht Club - Laser Eyes #3749",
         sku: "3",
-        image: "/images/camera.jpg"
+        image: "/images/bayc-3749.jpg"
     },
     {
-        title: "Sony Playstation 4 Pro White Version",
+        title: "Bored Ape Yacht Club - Crown #8585",
         sku: "4",
-        image: "/images/playstation.jpg"
+        image: "/images/bayc-8585.jpg"
     },
     {
-        title: "Logitech G-Series Gaming Mouse",
+        title: "Bored Ape Yacht Club - Party #8817",
         sku: "5",
-        image: "/images/mouse.jpg",
+        image: "/images/bayc-8817.jpg",
     },
     {
-        title: "Amazon Echo Dot 3rd Generation",
+        title: "Bored Ape Yacht Club x Adidas",
         sku: "6",
-        image: "/images/alexa.jpg"
+        image: "/images/bayc-adidas.jpg"
+    },
+    {
+        title: "Bored Ape Yacht Club - Knife #8135",
+        sku: "7",
+        image: "/images/bayc-8135.jpg"
+    },
+    {
+        title: "Bored Ape Yacht Club - Suit #3562",
+        sku: "8",
+        image: "/images/bayc-3562.jpg"
     },
 ];
 
